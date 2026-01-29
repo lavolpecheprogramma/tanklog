4be6545d8aa5005b35064378ce88a4fbfbd63cc5
Alberto Parziale <alberto.parziale@mono-grid.com>
2026-01-29 22:12:40 +0100

    fix: make dialogs scrollable

--- components/ui/dialog/DialogContent.vue
+++ components/ui/dialog/DialogContent.vue
@@ -31,7 +31,7 @@ const { t } = useI18n()
       v-bind="forwarded"
       :class="
         cn(
-          'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-border bg-background p-6 shadow-lg duration-200 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 sm:rounded-lg',
+          'fixed left-[50%] top-[50%] z-50 grid w-full max-h-[100dvh] max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 overflow-y-auto border border-border bg-background p-6 shadow-lg duration-200 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 sm:rounded-lg',
           props.class,
         )
       "
